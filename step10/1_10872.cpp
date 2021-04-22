d52ffd6de99b2a50007ca3087b79d1bf8082bcbf
ahyeonjoung <ribbon0529@naver.com>
2021-04-22 16:19:44 +0900

    step10 update

---
+++ step10/1_10872.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int a) {
+	if (a == 1) {
+		return a;
+	}
+	else {
+		return a * factorial(a - 1);
+	}
+	
+}
+
+int main() {
+	int num;
+	cin >> num;
+	if (num == 0)
+		cout << 1;
+	else {
+		int result=factorial(num);
+		cout << result;
+	}
+
+}
---
+++ step10/2_10870.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int a) {
+	if (a == 0) {
+		return a;
+	}
+	else if (a == 1) {
+		return 1;
+	}
+	else {
+		return fibonacci(a - 1) + fibonacci(a - 2);
+	}
+}
+
+
+int main() {
+	int num;
+	cin >> num;
+	cout << fibonacci(num);
+
+}
